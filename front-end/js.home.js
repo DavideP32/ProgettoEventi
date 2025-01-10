a934d8d92874aa5c107657382f00ad1695ce7ab3
PaolaVotano <paolavotano56@gmail.com>
2025-01-10 19:33:29 +0100

    aggiornamenti home

---
+++ front-end/js.home.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    let navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
