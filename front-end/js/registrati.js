93bbe400f4926429256d4ba7d9ab5e5dcc796f4b
Gerardo Tozio <sevengery9@gmail.com>
2025-01-13 12:31:37 +0100

    Login

--- front-end/js/registrati.js
+++ front-end/js/registrati.js
@@ -1,16 +1,3 @@
-// //Messaggio di errore per password non confermata
-// function validatePassword() {
-//     const password = document.getElementById('password').value;
-//     const confirmPassword = document.getElementById('confirm-password').value;
-//     const errorElement = document.getElementById('password-error');
-//     if (password !== confirmPassword) {
-//         errorElement.style.display = 'block';
-//     } else {
-//         errorElement.style.display = 'none';
-//         alert('Registrazione completata!');
-//     }
-// }
-
 // Richiamo il form
 const form = document.querySelector('form');
 
