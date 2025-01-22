6ad0f4d858e7a7c49ff38451c078d379371a40b0
Davide <183734626+DavideP32@users.noreply.github.com>
2025-01-22 23:06:47 +0100

    .

--- front-end/js/prenotazioni.js
+++
@@ -1,7 +0,0 @@
-    // Calcolo Prezzo Totale Dinamico
-    const prezzoPerPersona = 50;
-    document.getElementById('numPersone').addEventListener('input', function() {
-        const persone = this.value;
-        const totale = persone * prezzoPerPersona;
-        document.getElementById('totalPrice').textContent = totale;
-    });
